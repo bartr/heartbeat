d47ccc6099cba2b9156105af92d3401acccd63d8
bartr <bartr@microsoft.com>
2021-06-13 03:17:09 +0000

    updated post install

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # install webv
-dotnet tool install -g webvalidate --version 2.0.0-beta3
+dotnet tool install -g webvalidate --version 2.0.0-beta2
 
 # add environment specific commands here
