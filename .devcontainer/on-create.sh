042df45e81f55071a21595adee1fd3165e244529
bartr <bartr@microsoft.com>
2021-09-19 04:01:44 +0000

    updated script

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -5,5 +5,6 @@ echo "on-create start" >> ~/status
 # this runs when container is initially created
 
 # add your commands here
+go get -v golang.org/x/tools/gopls
 
 echo "on-create complete" >> ~/status
