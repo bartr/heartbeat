969b956d790ddff0510ef43b7d8c71a688874881
Bart Robertson <bartr@microsoft.com>
2024-01-15 00:54:30 +0000

    updated Codespaces

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -4,7 +4,7 @@ echo "on-create start" >> ~/status
 
 echo "export GOPATH='$HOME/go'" >> $HOME/.zshrc
 
-go install golang.org/x/tools/gopls@latest
+# go install golang.org/x/tools/gopls@latest
 
 # pull docker base images
 docker pull golang:alpine
